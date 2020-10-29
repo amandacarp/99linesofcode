ea553b8a548517a884897638dd5053f828d2a3f9
Amanda Carpentieri <mandac@Amandas-MacBook-Air.local>
2020-10-29 13:44:21 -0700

    added lyrics code

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 var friends = ["Alexandra", "Alex", "Carolyn", "Roger", "Adam"];
+
+for(var i = 0; i < friends.length; i++){
+    console.log(friends[i]+":");
+        for (var j = 99; j >=1; j--){
+            if (j > 2){
+                console.log([j] + "lines of code in the file," + [j] + "lines of code;" + friends[i] + "strikes one out, clears it all out," + (j-1) + "lines of code in the file");
+            } else if (j == 2) {
+                console.log([j] + "lines of code in the file," + [j] + "lines of code;" + friends[i] + "strikes one out, clears it all out," + (j -1) + "lines of code in the file");
+            } else if(j == 1) {
+                console.log([j] + "line of code in the file," + [j] + "line of code;" + friends[i] + "strikes one out, clears it all out, no more lines of code in the file");
+            }
+        }
+}
